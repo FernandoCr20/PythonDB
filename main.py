205c594e1155b0263a997e42e6547e065b9c4b04
FERNANDO CHAVES RODRIGUES <202308423991@alunos.estacio.br>
2024-03-22 08:39:55 -0300

    att3

--- main.py
+++ main.py
@@ -33,15 +33,33 @@ cursor.execute(''' CREATE TABLE IF NOT EXISTS Veiculo(
 
 # cursor.execute(''' ALTER TABLE Veiculo ADD motor REAL; ''')
 
-pessoa = Pessoa(12345678900, 'Fernando', '29-12-2002', False)
+# pessoa = Pessoa(12345678900, 'Fernando', '29-12-2002', False)
 
 # comando = ''' INSERT INTO Pessoa(cpf, nome, nascimento, oculos) VALUES (?,?,?,?); '''
 # cursor.execute(comando, (pessoa.cpf, pessoa.nome, pessoa.nascimento, pessoa.usa_oculos))
 
-pessoas = [Pessoa(98765432100, 'Pedro', '15-03-2000', True), Pessoa(78945613200, 'Ana', '01-01-2001', False)]
-comando = ''' INSERT INTO Pessoa(cpf, nome, nascimento, oculos) VALUES(?,?,?,?); '''
+# pessoas = [Pessoa(98765432100, 'Pedro', '15-03-2000', True), Pessoa(78945613200, 'Ana', '01-01-2001', False)]
+# comando = ''' INSERT INTO Pessoa(cpf, nome, nascimento, oculos) VALUES(?,?,?,?); '''
 
-cursor.executemany(comando, [(i.cpf, i.nome, i.nascimento, i.usa_oculos) for i in pessoas])
+# cursor.executemany(comando, [(i.cpf, i.nome, i.nascimento, i.usa_oculos) for i in pessoas])
+
+# ---------------------------------------------------------------------------------------------------------------------------
+
+# comando = ''' INSERT INTO Marca(nome, sigla) VALUES (:nome, :sigla); '''
+# marcaA = Marca("Marca A", "MA")
+
+# cursor.execute(comando, vars(marcaA))
+# marcaA.id = cursor.lastrowid
+
+# marcaB = Marca("Marca B", "MB")
+# cursor.execute(comando, vars(marcaB))
+# marcaB.id = cursor.lastrowid
+
+comando = ''' INSERT INTO Veiculo(placa, ano, cor, motor, proprietario, marca) VALUES (:placa, :ano, :cor, :motor, :proprietario, :marca); '''
+veiculo1 = Veiculo("AABB001", "2001", "Prata", 1.0, 12345678900, 1)
+veiculo2 = Veiculo("BABB001", "2002", "Preto", 1.5, 78945613200, 2)
+cursor.execute(comando, vars(veiculo1))
+cursor.execute(comando, vars(veiculo2))
 
 banco.commit()
 cursor.close()
